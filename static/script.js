1aae9d57b64ce7acf7294765277c160acf9092f6
Triplejw <wonderjj2017@gmail.com>
2023-06-16 00:05:47 +0800

    Added reset and Changed submit action

--- static/script.js
+++ static/script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     // Adjust the height of the background animation element
     var windowHeight = $(window).height();
     $('#background-animation').height(windowHeight);
+
+    // Reset the form when the reset button is clicked
+    $('#reset-button').click(function() {
+        $('#credit-calculator-form')[0].reset();
+    });
 });
